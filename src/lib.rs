00a45aa5cc969cb0b10346193e1632d08d32bb56
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2022-12-01 18:58:11 +0100

    📝 remove code example

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,6 @@ use std::{
 /// Read a file into an iterator of lines
 /// # Arguments
 /// * `filename` - The name of the file to read
-/// # Example
-/// ```
-/// let lines = read_lines("input.txt");
-/// ```
 /// # Returns
 /// An iterator of lines
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
